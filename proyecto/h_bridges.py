c37719c998e72b14fce8727b315a84248fa05a2f
Samuel Boza <samuelboza18102003@gmail.com>
2021-11-20 17:49:26 -0400

    H_Bridge Correction

--- proyecto/h_bridges.py
+++ proyecto/h_bridges.py
@@ -1,31 +1,32 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
+import gpiozero
 
-class Transistor():
-    GPIO.setmode(GPIO.BOARD)
+# class Transistor():
+#     GPIO.setmode(GPIO.BOARD)
     
-    def __init__(self, pin):
-        self.pin = pin
+#     def __init__(self, pin):
+#         self.pin = pin
 
-    def activate(self, output):
-        assert self.pin is not None
-        assert output is not None
-        GPIO.setup(self.pin, GPIO.OUT)
+#     def activate(self, output):
+#         assert self.pin is not None
+#         assert output is not None
+#         GPIO.setup(self.pin, GPIO.OUT)
 
-        if(output == "HIGH"):
-            GPIO.output(self.pin, GPIO.HIGH)
+#         if(output == "HIGH"):
+#             GPIO.output(self.pin, GPIO.HIGH)
 
-        elif(output == "LOW"):
-            GPIO.output(self.pin, GPIO.LOW)
+#         elif(output == "LOW"):
+#             GPIO.output(self.pin, GPIO.LOW)
 
-class Puente_h():
+class H_Bridges():
     def __init__(self, pin1, pin2):
-        self.opener = Transistor(pin = pin1)
-        self.closer = Transistor(pin = pin2)
-        self.high_opener = self.opener.activate(output = "HIGH")
-        self.low_opener = self.opener.activate(output = "LOW")
-        self.high_closer = self.closer.activate(output = "HIGH")
-        self.low_closer = self.closer.activate(output = "LOW")
+        self.opener = gpiozero.OutputDevice(pin1, active_high=True, initial_value=False)
+        self.opener = gpiozero.OutputDevice(pin1, active_high=True, initial_value=False)
+        self.high_opener = self.opener.on()
+        self.low_opener = self.opener.off()
+        self.high_closer = self.closer.on()
+        self.low_closer = self.closer.off()
 
     def opens(self, timeout = 3):
         """Function to open the door"""
