3cc7ff4647fca6a86531f35e5657339c620e96d0
Samuel Boza <samuelboza18102003@gmail.com>
2021-11-20 18:41:54 -0400

    Doors

--- proyecto/h_bridges.py
+++ proyecto/h_bridges.py
@@ -1,39 +1,26 @@
-# import RPi.GPIO as GPIO
 import time
 import gpiozero
-
-# class Transistor():
-#     GPIO.setmode(GPIO.BOARD)
-    
-#     def __init__(self, pin):
-#         self.pin = pin
-
-#     def activate(self, output):
-#         assert self.pin is not None
-#         assert output is not None
-#         GPIO.setup(self.pin, GPIO.OUT)
-
-#         if(output == "HIGH"):
-#             GPIO.output(self.pin, GPIO.HIGH)
-
-#         elif(output == "LOW"):
-#             GPIO.output(self.pin, GPIO.LOW)
 
 class H_Bridge():
     def __init__(self, pin1, pin2):
         self.opener = gpiozero.OutputDevice(pin1, active_high=True, initial_value=False)
         self.closer = gpiozero.OutputDevice(pin2, active_high=True, initial_value=False)
 
-    def opens(self, timeout = 0.1):
-        """Function to open the door"""
-        self.opener.on()
-        time.sleep(timeout)
-        self.opener.off()
-        return True
+class Doors():
+    def __init__(self, open1, close1, open2, close2):
+        self.door1 = H_Bridge(open1, close1)
+        self.door2 = H_Bridge(open2, close2)
 
-    def close(self, timeout = 0.1):
-        """Function to close the doors"""
-        self.closer.on()
+    def opening(self, timeout):
+        self.door1.opener.on()
+        self.door2.opener.on()
         time.sleep(timeout)
-        self.closer.off()
-        return True
+        self.door1.opener.off()
+        self.door2.opener.off()
+    
+    def closing(self, timeout):
+        self.door1.closer.on()
+        self.door2.closer.on()
+        time.sleep(timeout)
+        self.door1.closer.off()
+        self.door2.closer.off()        
